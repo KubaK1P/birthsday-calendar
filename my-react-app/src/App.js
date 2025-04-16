8f75b166eadce045587d98ff2766aa6c0d79e4c8
KubaK1P <1p22kus@gmail.com>
2025-04-16 22:16:54 +0200

    feat: calendar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const year = 2025;
+  const month = 3;
+  const birthsDay = 26;
+  const birthsDate = new Date(year, month, birthsDay);
+  const currentDate = new Date();
+  const currentDay = currentDate.getDate();
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  // const monthName = birthsDate.toLocaleString('default', { month: 'long' });
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const startDay = new Date(year, month, 1).getDay();
+  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const calendarCells = [];
+
+for (let i = 0; i < startDay; i++) {
+  calendarCells.push(<div key={`empty-${i}`} className="day empty"></div>);
+}
+
+for (let day = 1; day <= daysInMonth; day++) {
+  calendarCells.push(
+    <div key={`day-${day}`} className={`day ${(day === birthsDay)? "birthsday" : ""} ${(day === currentDay)? "current" : ""} ${(day > currentDay && day < birthsDay)? "remaining" : ""} `}>
+      <span>{day}</span>
+    </div>
+  );
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Withouto uno problemo
-        </p>
+    <div className="container">
+      <header className="app-header">
+        <h1>Birthsday calendar: {birthsDate.toLocaleDateString("en-EN", options)}</h1>
+        <h2><span className="red">{birthsDay - currentDay}</span> days remaining</h2>
       </header>
+      <main className="app-main">
+        <div className="calendar">
+          {(dayNames.map((day) => {
+            return <div key={day} className="day-label">{day}</div>
+          }))}
+          {calendarCells}
+        </div>
+      </main>
+    
     </div>
   );
 }
